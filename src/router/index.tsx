e7a8819fee5e62577e9637e815517e8d23959d00
金戈宁 <626723063@qq.com>
2023-03-26 12:37:19 +0800

    fix: 代码修复

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,8 @@
-import { createHashRouter, Navigate } from "react-router-dom";
-import type { RouteObject } from "react-router-dom";
-import React from "react";
+import { createHashRouter, Navigate } from 'react-router-dom';
+import type { RouteObject } from 'react-router-dom';
+import React from 'react';
 // 懒加载的方式引入对应页面 配置suspense在index.tsx
-import { lazy } from "react";
+import { lazy } from 'react';
 // icons
 import {
   CopyOutlined,
@@ -10,17 +10,17 @@ import {
   WarningOutlined,
   FileAddOutlined,
   ScheduleOutlined,
-} from "@ant-design/icons";
-import { AntdIconProps } from "@ant-design/icons/lib/components/AntdIcon";
+} from '@ant-design/icons';
+import { AntdIconProps } from '@ant-design/icons/lib/components/AntdIcon';
 // 使用lazy中的回调函数导入页面路径
-const Home = lazy(() => import("../views/Home/Home"));
-const Sign = lazy(() => import("../views/Sign/Sign"));
-const Check = lazy(() => import("../views/Check/Check"));
-const Login = lazy(() => import("../views/Login/Login"));
+const Home = lazy(() => import('../views/Home/Home'));
+const Sign = lazy(() => import('../views/Sign/Sign'));
+const Check = lazy(() => import('../views/Check/Check'));
+const Login = lazy(() => import('../views/Login/Login'));
 // 懒加载的形式引入
-const BeforeEach = lazy(() => import("../components/BeforeEach/BeforeEach"));
+const BeforeEach = lazy(() => import('../components/BeforeEach/BeforeEach'));
 // 扩展d.ts文件中 react-router 中RouteObject的（IndexRouteObject || NonIndexRouteObject）两个接口
-declare module "react-router" {
+declare module 'react-router' {
   interface IndexRouteObject {
     meta?: {
       menu?: boolean;
@@ -44,57 +44,66 @@ declare module "react-router" {
     name: string;
   }
 }
+
 // 定义路由表组及类型
 export const routes: RouteObject[] = [
   {
     // 首页重定向到第一个二级路由
-    path: "/",
+    path: '/',
     element: <Navigate to="/sign" />,
-    name: "/",
+    name: '/',
   },
   {
-    path: "/",
+    path: '/',
     element: (
       <BeforeEach>
         <Home />
       </BeforeEach>
     ),
-    name: "home",
+    name: 'home',
     meta: {
       menu: true,
-      title: "主页",
-      icon: React.createElement(CopyOutlined),
+      title: '主页',
+      // icon: React.createElement(CopyOutlined),
+      icon: <CopyOutlined />,
       auth: true,
     },
     children: [
       {
-        path: "sign",
+        path: 'sign',
         element: <Sign />,
         meta: {
           menu: true,
-          title: "标记",
-          icon: React.createElement(CalendarOutlined),
+          title: '标记',
+          // icon: React.createElement(CalendarOutlined),
+          icon: <CalendarOutlined />,
           auth: true,
         },
-        name: "sign",
+        name: 'sign',
       },
       {
-        path: "check",
+        path: 'check',
         element: <Check />,
         meta: {
           menu: true,
-          title: "核实",
-          icon: React.createElement(ScheduleOutlined),
+          title: '核实',
+          // icon: React.createElement(ScheduleOutlined),
+          icon: <ScheduleOutlined />,
           auth: true,
         },
-        name: "check",
+        name: 'check',
       },
     ],
   },
   {
-    path: "/login",
-    element: React.createElement(BeforeEach, null, React.createElement(Login)),
-    name: "login",
+    path: '/login',
+    // element: React.createElement(BeforeEach, null, React.createElement(Login)),
+    element: (
+      <BeforeEach>
+        <Login />
+      </BeforeEach>
+    ),
+    name: 'login',
   },
 ];
 // 创建路由对象,createBrowserRouter(路由表)
