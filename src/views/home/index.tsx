5dce35081890a97a5fcf86382a5f6f1a034e7670
IngaFred <116004221+IngaFred@users.noreply.github.com>
2023-03-28 00:33:25 +0800

    feat: personal的得到学生信息

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
 // 首页（公告，主题分类，课程列表，我的作业，个人信息）
 // 洪浩然，章徐松
+
 export default function Home() {
   return (
     <div>Home</div>
   )
 }
-
--- src/views/personal/index.tsx
+++ src/views/personal/index.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
-import styles from './index.module.scss';
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+import styles from "./index.module.scss";
 // 个人信息（设置密码）
 // 蔡启航
+
 export default function Personal() {
+  // 得到学生信息
+  const student = useSelector((state: RootState) => state.user.infos);
+  console.log(student);
+
   return <div>Personal</div>;
 }
