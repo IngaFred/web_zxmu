3ea171b8cb0319cbd3fa9c461ecc9bb779a72449
Yhq <1193086708@qq.com>
2023-03-28 15:27:14 +0800

    feat：添加了课程大纲,及评论区模块的初步展示

--- src/views/student/course/index.tsx
+++ src/views/student/course/index.tsx
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from "react";
 import { Layout, Image, Card } from "antd";
 import styles from "./index.module.scss";
 import http from "../../../utils/http";
+import { title } from "process";
+import Discussion from "./discussion";
 const { Header, Content, Footer } = Layout;
 
 //接受参数接口
@@ -38,41 +40,75 @@ export default function Course() {
     };
   }, []);
 
+  //头部模块(课程名、任课教师、课程封面、课程简介)
   const showLessonInfo = () => {
     return (
-      <div className={styles.box}>
-        {/* <div>
-          <p>{testLessonInfo.name}</p>
-        </div> */}
-        <div></div>
-        <Image
-          style={{ width: "450px", height: "320px", borderRadius: "5px" }}
-          src={testLessonInfo.picFile as string}
-        />
-        <Card className={styles.card}>
-          <p>{testLessonInfo.info}</p>
+      <div>
+        <div className={styles.title}>
+          <div>
+            <h1>课程名：{testLessonInfo.name}</h1>
+          </div>
+          <div>
+            <h1>任课教师：王老师</h1>
+          </div>
+        </div>
+        <div className={styles.box}>
+          <Image
+            style={{ width: "450px", height: "320px", borderRadius: "5px" }}
+            src={testLessonInfo.picFile as string}
+          />
+          <Card className={styles.card}>
+            <p>{testLessonInfo.info}</p>
+          </Card>
+        </div>
+      </div>
+    );
+  };
+  //内容模块(教学大纲)
+  const showContent = () => {
+    return (
+      <div className={styles.outline}>
+        <div className={styles.outlineTitle}>
+          <h1>教学大纲</h1>
+        </div>
+        <Card className={styles.outlineCard}>
+          <div className={styles.outlineCardTitle}>
+            <h2>标题</h2>
+          </div>
+          <div className={styles.outlineCardContent}>
+            <p>内容</p>
+          </div>
         </Card>
       </div>
     );
   };
 
+  //讨论区
+  // const showDiscussion = () => {
+  //   return (
+  //     <div className={styles.discussion}>
+  //       <h1>评论</h1>
+  //       <div className={styles.discussionUser}>
+  //         <img src={testLessonInfo.picFile} alt="未登录时图片" />
+  //         <span>看完了，登录一下分享感受吧！</span>
+  //       </div>
+  //     </div>
+  //   );
+  // };
+
+  //作业列表
   return (
     <Layout>
-      <Header className={styles.header}>
-        {/* <Image
-            style={{ width: "450px", height: "320px", borderRadius: "5px" }}
-            src={imgSrc}
-          />
-          <Card className={styles.card}>
-            <p>{}</p>
-          </Card> */}
-        {showLessonInfo()}
-      </Header>
-      <Content>
-        <p>Lesson</p>
-      </Content>
-      <Footer>
-        <p>Footer</p>
+      <Header className={styles.header}>{showLessonInfo()}</Header>
+      <Content>{showContent()}</Content>
+      <Footer className={styles.footer}>
+        <Discussion
+          userAvatar={""}
+          userName={""}
+          content={""}
+          likes={0}
+          replyCount={0}
+        ></Discussion>
       </Footer>
     </Layout>
   );
