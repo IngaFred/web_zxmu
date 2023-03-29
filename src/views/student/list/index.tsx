43719ff2c7c921a620b669321e0a4d287ce62189
IngaFred <116004221+IngaFred@users.noreply.github.com>
2023-03-30 00:05:10 +0800

    fix: Card使用修改

--- src/views/student/list/index.tsx
+++ src/views/student/list/index.tsx
@@ -1,14 +1,15 @@
 //@ts-nocheck
 import React, { useEffect, useState } from "react";
-import { message, Avatar, List, Card } from "antd";
-
+import { message, Card, Button, Row, Col } from "antd";
 import styles from "./index.module.scss";
 import { getLessons } from "../../../service/list";
+import Meta from "antd/es/card/Meta";
+import { useNavigate } from "react-router-dom";
+
 // 我的作业列表（作业标题，课程名字，老师名字，展示分数，批改状态）
 // 娄竞楷
 export default function ClassList() {
   const [lessonAll, setLessonAll] = useState<[]>([]);
-  // const [lessonName,setLesssonName] = useState<[]>([]);
   useEffect(() => {
     getLessons().then((ret) => {
       if (ret.data.success) {
@@ -21,40 +22,58 @@ export default function ClassList() {
     return () => {};
   }, []);
 
-  console.log(lessonAll);
-
-  const data = [
-    {
-      title: "Title 1",
-    },
-  ];
-
-  // const lessNameList = lessonAll.map((item) => (item.lessonName));
+  const navigate = useNavigate();
+  const handleMyCourse = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(e);
+    navigate("/course");
+  };
+  const handleMyDetail = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(e);
+    navigate("/detail");
+  };
 
   return (
-    <div className={styles.sc}>
-      {lessonAll.map((item, index) => (
-        <div key={index} >
-          {/* <List
-            grid={{ gutter: 16, column: 4 }}
-            dataSource={lessNameList}
-            renderItem={(item) => (
-              <List.Item>
-                <Card title={item.title}>Card content</Card>
-              </List.Item>
-            )}
-          /> */}
-            <Card  className={styles.rr} style={{backgroundImage:item.picUrl}} title={item.lessonName}>
-              <div>
-              <div>{item.info}</div>
-              </div>
-              <div>
-                <img src={item.picUrl} alt="" style={{width:'150px',height:'50px'}}/>
-              </div>
-              
+    <div className={styles.all}>
+      <Row gutter={24}>
+        {lessonAll.map((item, index) => (
+          <Col span={8}>
+            <Card
+              key={index}
+              size="small"
+              className={styles.card}
+              cover={
+                <img
+                  src={item.picUrl}
+                  alt=""
+                  style={{ width: "300px", height: "180px", padding: "10px" }}
+                />
+              }
+              actions={[
+                <Row justify={"space-between"}>
+                  <Button
+                    className={styles.rowBtn}
+                    onClick={(e) => handleMyCourse(item.lessonId, e)}
+                  >
+                    课程详情
+                  </Button>
+                  <Button
+                    className={styles.rowBtn}
+                    onClick={(e) => handleMyDetail(item.lessonId, e)}
+                  >
+                    我的作业
+                  </Button>
+                </Row>,
+              ]}
+            >
+              <Meta
+                title={item.lessonName}
+                description={item.info}
+                style={{ height: "80px" }}
+              />
             </Card>
-          </div>
-      ))}
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 }
