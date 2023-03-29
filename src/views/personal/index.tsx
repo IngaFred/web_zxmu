461da19825468022004fe61ce4238a7155185e8d
cqhcqh1206 <cqhcqh1206@163.com>
2023-03-29 14:21:59 +0800

    ci personal

--- src/views/personal/index.tsx
+++ src/views/personal/index.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
 import styles from "./index.module.scss";
+import { getUserInfo } from "../../service/personal";
 // 个人信息（设置密码）
 // 蔡启航
 import type { CascaderProps } from "antd";
@@ -25,41 +26,6 @@ interface DataNodeType {
   label: string;
   children?: DataNodeType[];
 }
-
-const residences: CascaderProps<DataNodeType>["options"] = [
-  {
-    value: "zhejiang",
-    label: "Zhejiang",
-    children: [
-      {
-        value: "hangzhou",
-        label: "Hangzhou",
-        children: [
-          {
-            value: "xihu",
-            label: "West Lake",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    value: "jiangsu",
-    label: "Jiangsu",
-    children: [
-      {
-        value: "nanjing",
-        label: "Nanjing",
-        children: [
-          {
-            value: "zhonghuamen",
-            label: "Zhong Hua Men",
-          },
-        ],
-      },
-    ],
-  },
-];
 
 const formItemLayout = {
   labelCol: {
@@ -86,29 +52,17 @@ const tailFormItemLayout = {
 };
 
 const App: React.FC = () => {
+  const [userInfo,setUserTnfo]=useState<any>({});
+
+  useEffect(()=>{
+    getUserInfo().then();
+  },[]);
+
   const [form] = Form.useForm();
 
   const onFinish = (values: any) => {
     console.log("Received values of form: ", values);
   };
-
-  const prefixSelector = (
-    <Form.Item name="prefix" noStyle>
-      <Select style={{ width: 70 }}>
-        <Option value="86">+86</Option>
-        <Option value="87">+87</Option>
-      </Select>
-    </Form.Item>
-  );
-
-  const suffixSelector = (
-    <Form.Item name="suffix" noStyle>
-      <Select style={{ width: 70 }}>
-        <Option value="USD">$</Option>
-        <Option value="CNY">¥</Option>
-      </Select>
-    </Form.Item>
-  );
 
   const [autoCompleteResult, setAutoCompleteResult] = useState<string[]>([]);
 
@@ -133,24 +87,18 @@ const App: React.FC = () => {
       form={form}
       name="register"
       onFinish={onFinish}
-      initialValues={{
-        residence: ["zhejiang", "hangzhou", "xihu"],
-        prefix: "86",
-      }}
       style={{ maxWidth: 600 }}
       scrollToFirstError
     >
       <Form.Item
-        name="email"
-        label="E-mail"
+        name="nickname"
+        label="修改昵称"
+        tooltip="What do you want others to call you?"
         rules={[
           {
-            type: "email",
-            message: "The input is not valid E-mail!",
-          },
-          {
             required: true,
-            message: "Please input your E-mail!",
+            message: "Please input your nickname!",
+            whitespace: true,
           },
         ]}
       >
@@ -158,8 +106,42 @@ const App: React.FC = () => {
       </Form.Item>
 
       <Form.Item
+        name="phone"
+        label="学号"
+        rules={[{ required: true, message: "Please input your phone number!" }]}
+      >
+        <Input style={{ width: "100%" }} />
+      </Form.Item>
+
+      {/* <Form.Item
+        name="website"
+        label="Website"
+        rules={[{ required: true, message: "Please input website!" }]}
+      >
+        <AutoComplete
+          options={websiteOptions}
+          onChange={onWebsiteChange}
+          placeholder="website"
+        >
+          <Input />
+        </AutoComplete>
+      </Form.Item> */}
+
+      <Form.Item
+        name="gender"
+        label="性别"
+        /* rules={[{ required: true, message: "Please select gender!" }]} */
+      >
+        <Select placeholder="select your gender">
+          <Option value="male">Male</Option>
+          <Option value="female">Female</Option>
+          <Option value="other">Other</Option>
+        </Select>
+      </Form.Item>
+
+      <Form.Item
         name="password"
-        label="Password"
+        label="新的密码"
         rules={[
           {
             required: true,
@@ -173,7 +155,7 @@ const App: React.FC = () => {
 
       <Form.Item
         name="confirm"
-        label="Confirm Password"
+        label="确认新的密码"
         dependencies={["password"]}
         hasFeedback
         rules={[
@@ -196,111 +178,17 @@ const App: React.FC = () => {
         <Input.Password />
       </Form.Item>
 
-      <Form.Item
-        name="nickname"
-        label="Nickname"
-        tooltip="What do you want others to call you?"
-        rules={[
-          {
-            required: true,
-            message: "Please input your nickname!",
-            whitespace: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        name="residence"
-        label="Habitual Residence"
-        rules={[
-          {
-            type: "array",
-            required: true,
-            message: "Please select your habitual residence!",
-          },
-        ]}
-      >
-        <Cascader options={residences} />
-      </Form.Item>
-
-      <Form.Item
-        name="phone"
-        label="Phone Number"
-        rules={[{ required: true, message: "Please input your phone number!" }]}
-      >
-        <Input addonBefore={prefixSelector} style={{ width: "100%" }} />
-      </Form.Item>
-
-      <Form.Item
-        name="donation"
-        label="Donation"
-        rules={[{ required: true, message: "Please input donation amount!" }]}
-      >
-        <InputNumber addonAfter={suffixSelector} style={{ width: "100%" }} />
-      </Form.Item>
-
-      <Form.Item
-        name="website"
-        label="Website"
-        rules={[{ required: true, message: "Please input website!" }]}
-      >
-        <AutoComplete
-          options={websiteOptions}
-          onChange={onWebsiteChange}
-          placeholder="website"
-        >
-          <Input />
-        </AutoComplete>
-      </Form.Item>
+      
 
       <Form.Item
         name="intro"
-        label="Intro"
+        label="自我介绍"
         rules={[{ required: true, message: "Please input Intro" }]}
       >
         <Input.TextArea showCount maxLength={100} />
       </Form.Item>
 
-      <Form.Item
-        name="gender"
-        label="Gender"
-        rules={[{ required: true, message: "Please select gender!" }]}
-      >
-        <Select placeholder="select your gender">
-          <Option value="male">Male</Option>
-          <Option value="female">Female</Option>
-          <Option value="other">Other</Option>
-        </Select>
-      </Form.Item>
-
-      <Form.Item
-        label="Captcha"
-        extra="We must make sure that your are a human."
-      >
-        <Row gutter={8}>
-          <Col span={12}>
-            <Form.Item
-              name="captcha"
-              noStyle
-              rules={[
-                {
-                  required: true,
-                  message: "Please input the captcha you got!",
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Button>Get captcha</Button>
-          </Col>
-        </Row>
-      </Form.Item>
-
-      <Form.Item
+      {/* <Form.Item
         name="agreement"
         valuePropName="checked"
         rules={[
@@ -316,7 +204,7 @@ const App: React.FC = () => {
         <Checkbox>
           I have read the <a href="">agreement</a>
         </Checkbox>
-      </Form.Item>
+      </Form.Item> */}
       <Form.Item {...tailFormItemLayout}>
         <Button type="primary" htmlType="submit">
           Register
@@ -335,7 +223,6 @@ export default function Personal() {
       <div>
         <App />
       </div>
-      <div>Personal</div>
     </>
   );
 }
