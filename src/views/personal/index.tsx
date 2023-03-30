9630f1c7b3e3a169952d7a906436582190fcfbd7
cqhcqh1206 <cqhcqh1206@163.com>
2023-03-30 12:13:55 +0800

    ci personal

--- src/views/personal/index.tsx
+++ src/views/personal/index.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
-import { Button, Divider } from "antd";
+import { Button, Modal, Divider, Space } from "antd";
+import { ExclamationCircleOutlined } from "@ant-design/icons";
 import styles from "./index.module.scss";
-import ModalComponent from './modal';
+import LocalizedModal from "./modal";
 import { getUserInfo } from "../../service/personal";
 // 个人信息（设置密码）
 // 蔡启航
@@ -12,18 +13,18 @@ export default function Personal() {
   // 得到学生信息
   const student = useSelector((state: RootState) => state.user.infos);
   console.log(student);
-  
-  const [modalVisible, setModalVisible] = useState(false);
-  const handleOpenModal = () => {
-    setModalVisible(true);
-  };
-  const handleOk = () => {
-    console.log('Clicked OK');
-  };
-  const handleCancel = () => {
-    console.log('Clicked Cancel');
-  };
-  
+
+  const [modal, contextHolder] = Modal.useModal();
+  /* const confirm = () => {
+    modal.confirm({
+      title: "Confirm",
+      icon: <ExclamationCircleOutlined />,
+      content: "Bla bla ...",
+      okText: "确认",
+      cancelText: "取消",
+    });
+  }; */
+
   return (
     <>
       <h1>个人信息</h1>
@@ -35,30 +36,18 @@ export default function Personal() {
           alt="头像"
         />
         <button className={styles.fixtx} /* onClick={()=> }  */>
-        {"修改头像"}
-      </button>
+          {"修改头像"}
+        </button>
       </div>
 
       <div className={styles.shuchu}>学号：{111}</div>
       <div className={styles.shuchu}>姓名；{"我"}</div>
 
-      <button className={styles.fixmima} onClick={()=> handleOpenModal} >
-        {"修改密码"}
-      </button>
-      <ModalComponent
-        title="Modal Title"
-        content="Modal Content"
-        visible={modalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      />
-
-      {/* <div className={styles.modal} >
-        <div className={styles.modal2}>
-
-        </div>
-
-      </div> */}
+      <Space>
+        <LocalizedModal />
+        {/* <Button onClick={confirm}>Confirm</Button> */}
+      </Space>
+      {contextHolder}
     </>
   );
 }
