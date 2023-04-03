9a90933857f58e457c11b39d6f4960788fcc0e6b
IngaFred <116004221+IngaFred@users.noreply.github.com>
2023-04-03 12:37:09 +0800

    feat: 主页接口演示修改

--- src/service/home.ts
+++ src/service/home.ts
@@ -1,6 +1,12 @@
 import http from "../utils/http";
 
-export const getCourses = async () => {
-  const ret = await http.get("/lesson/simple");
+// 获取主题模块
+export const getModel = async () => {
+  const ret = await http.get("/lesson/model");
+  return ret;
+};
+// 获取对应主题模块下的课程
+export const getModelLessons = async (modelId: string) => {
+  const ret = await http.get("/lesson/model/lesson?modelId=" + modelId);
   return ret;
 };
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,10 +1,19 @@
 //@ts-nocheck
 import React, { useEffect, useState } from 'react';
 import styles from './index.module.scss';
-import { Divider, Carousel, Card, Row, message, Col, Button } from 'antd';
-import { getCourses } from '../../service/home';
+import {
+	Divider,
+	Carousel,
+	Card,
+	Row,
+	message,
+	Col,
+	Button,
+	Empty,
+} from 'antd';
 import { useNavigate } from 'react-router-dom';
 import Meta from 'antd/es/card/Meta';
+import { getModel, getModelLessons } from '../../service/home';
 
 // 首页（公告，主题分类，课程列表，我的作业，个人信息）
 // 洪浩然，章徐松
@@ -18,20 +27,330 @@ const contentStyle = {
 };
 
 export default function Home() {
-	const [Courses, setCourse] = useState([]);
+	const [model, setModel] = useState([]);
+	const [modelLessons1, setModelLessons1] = useState([]);
+	const [modelLessons2, setModelLessons2] = useState([]);
+	const [modelLessons3, setModelLessons3] = useState([]);
+	const [modelLessons4, setModelLessons4] = useState([]);
+	const [modelLessons5, setModelLessons5] = useState([]);
+	const [modelLessons6, setModelLessons6] = useState([]);
 
 	useEffect(() => {
-		getCourses().then((ret) => {
+		getModel().then((ret) => {
 			if (ret.data.success) {
-				message.success(ret.data.errorMsg);
-				setCourse(ret.data.data);
-				//显示回参
-				//console.log(Courses);
+				// message.success(ret.data.errorMsg);
+				setModel(ret.data.data);
+				for (let i = 0; i < ret.data.data.length; i++) {
+					const modelId = ret.data.data[i].modelId;
+					switch (i) {
+						case 0:
+							getModelLessons(modelId).then((ret) => {
+								if (ret.data.success) {
+									setModelLessons1(ret.data.data);
+								}
+							});
+							break;
+						case 1:
+							getModelLessons(modelId).then((ret) => {
+								if (ret.data.success) {
+									setModelLessons2(ret.data.data);
+								}
+							});
+							break;
+						case 2:
+							getModelLessons(modelId).then((ret) => {
+								if (ret.data.success) {
+									setModelLessons3(ret.data.data);
+								}
+							});
+							break;
+						case 3:
+							getModelLessons(modelId).then((ret) => {
+								if (ret.data.success) {
+									setModelLessons4(ret.data.data);
+								}
+							});
+							break;
+						case 4:
+							getModelLessons(modelId).then((ret) => {
+								if (ret.data.success) {
+									setModelLessons5(ret.data.data);
+								}
+							});
+							break;
+						case 5:
+							getModelLessons(modelId).then((ret) => {
+								if (ret.data.success) {
+									setModelLessons6(ret.data.data);
+								}
+							});
+							break;
+						default:
+							break;
+					}
+				}
 			} else {
-				message.error('获取课程失败');
+				message.error('获取model失败');
 			}
 		});
 	}, []);
+
+	const modelLessons = {
+		模块一: modelLessons1.map((item, index) => (
+			<Col span={8}>
+				<Card
+					key={index}
+					size="small"
+					className={styles.card}
+					cover={
+						<img
+							src={item.picUrl}
+							alt="课程图片"
+							style={{
+								width: '260px',
+								height: '180px',
+								padding: '10px',
+							}}
+						/>
+					}
+					actions={[
+						<Row justify={'space-between'}>
+							<Button
+								className={styles.rowBtn}
+								onClick={(e) => handleMyCourse(item.lessonId, e)}
+							>
+								课程详情
+							</Button>
+							<Button
+								className={styles.rowBtn}
+								onClick={(e) => handleMyDetail(item.lessonId, e)}
+							>
+								作业列表
+							</Button>
+						</Row>,
+					]}
+				>
+					<Meta
+						title={item.lessonName}
+						description={item.info}
+						style={{ height: '80px' }}
+					/>
+				</Card>
+			</Col>
+		)),
+		模块二: modelLessons2.map((item, index) => (
+			<Col span={8}>
+				<Card
+					key={index}
+					size="small"
+					className={styles.card}
+					cover={
+						<img
+							src={item.picUrl}
+							alt="课程图片"
+							style={{
+								width: '260px',
+								height: '180px',
+								padding: '10px',
+							}}
+						/>
+					}
+					actions={[
+						<Row justify={'space-between'}>
+							<Button
+								className={styles.rowBtn}
+								onClick={(e) => handleMyCourse(item.lessonId, e)}
+							>
+								课程详情
+							</Button>
+							<Button
+								className={styles.rowBtn}
+								onClick={(e) => handleMyDetail(item.lessonId, e)}
+							>
+								作业列表
+							</Button>
+						</Row>,
+					]}
+				>
+					<Meta
+						title={item.lessonName}
+						description={item.info}
+						style={{ height: '80px' }}
+					/>
+				</Card>
+			</Col>
+		)),
+		模块三: modelLessons3.map((item, index) => (
+			<Col span={8}>
+				<Card
+					key={index}
+					size="small"
+					className={styles.card}
+					cover={
+						<img
+							src={item.picUrl}
+							alt="课程图片"
+							style={{
+								width: '260px',
+								height: '180px',
+								padding: '10px',
+							}}
+						/>
+					}
+					actions={[
+						<Row justify={'space-between'}>
+							<Button
+								className={styles.rowBtn}
+								onClick={(e) => handleMyCourse(item.lessonId, e)}
+							>
+								课程详情
+							</Button>
+							<Button
+								className={styles.rowBtn}
+								onClick={(e) => handleMyDetail(item.lessonId, e)}
+							>
+								作业列表
+							</Button>
+						</Row>,
+					]}
+				>
+					<Meta
+						title={item.lessonName}
+						description={item.info}
+						style={{ height: '80px' }}
+					/>
+				</Card>
+			</Col>
+		)),
+		模块四: modelLessons4.map((item, index) => (
+			<Col span={8}>
+				<Card
+					key={index}
+					size="small"
+					className={styles.card}
+					cover={
+						<img
+							src={item.picUrl}
+							alt="课程图片"
+							style={{
+								width: '260px',
+								height: '180px',
+								padding: '10px',
+							}}
+						/>
+					}
+					actions={[
+						<Row justify={'space-between'}>
+							<Button
+								className={styles.rowBtn}
+								onClick={(e) => handleMyCourse(item.lessonId, e)}
+							>
+								课程详情
+							</Button>
+							<Button
+								className={styles.rowBtn}
+								onClick={(e) => handleMyDetail(item.lessonId, e)}
+							>
+								作业列表
+							</Button>
+						</Row>,
+					]}
+				>
+					<Meta
+						title={item.lessonName}
+						description={item.info}
+						style={{ height: '80px' }}
+					/>
+				</Card>
+			</Col>
+		)),
+		模块五: modelLessons5.map((item, index) => (
+			<Col span={8}>
+				<Card
+					key={index}
+					size="small"
+					className={styles.card}
+					cover={
+						<img
+							src={item.picUrl}
+							alt="课程图片"
+							style={{
+								width: '260px',
+								height: '180px',
+								padding: '10px',
+							}}
+						/>
+					}
+					actions={[
+						<Row justify={'space-between'}>
+							<Button
+								className={styles.rowBtn}
+								onClick={(e) => handleMyCourse(item.lessonId, e)}
+							>
+							{toLog(item.lessonId)}
+								课程详情
+							</Button>
+							<Button
+								className={styles.rowBtn}
+								onClick={(e) => handleMyDetail(item.lessonId, e)}
+							>
+								作业列表
+							</Button>
+						</Row>,
+					]}
+				>
+					<Meta
+						title={item.lessonName}
+						description={item.info}
+						style={{ height: '80px' }}
+					/>
+				</Card>
+			</Col>
+		)),
+		模块六: modelLessons6.map((item, index) => (
+			<Col span={8}>
+				<Card
+					key={index}
+					size="small"
+					className={styles.card}
+					cover={
+						<img
+							src={item.picUrl}
+							alt="课程图片"
+							style={{
+								width: '260px',
+								height: '180px',
+								padding: '10px',
+							}}
+						/>
+					}
+					actions={[
+						<Row justify={'space-between'}>
+							<Button
+								className={styles.rowBtn}
+								onClick={(e) => handleMyCourse(item.lessonId, e)}
+							>
+								课程详情
+							</Button>
+							<Button
+								className={styles.rowBtn}
+								onClick={(e) => handleMyDetail(item.lessonId, e)}
+							>
+								作业列表
+							</Button>
+						</Row>,
+					]}
+				>
+					<Meta
+						title={item.lessonName}
+						description={item.info}
+						style={{ height: '80px' }}
+					/>
+				</Card>
+			</Col>
+		)),
+		default: <Empty description="暂无课程" />,
+	};
 
 	const navigate = useNavigate();
 	const handleMyCourse = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -57,54 +376,22 @@ export default function Home() {
 			</Carousel>
 
 			{/* 课程主题 */}
-			<h1>某主题下所有课程</h1>
-			<Divider />
-
-			<Row gutter={(24, 8)}>
-				{Courses.map((item, index) => (
-					<Col span={6}>
-						<Card
-							key={index}
-							size='small'
-							className={styles.card}
-							cover={
-								<img
-									src={item.picUrl}
-									alt=''
-									style={{ width: '260px', height: '180px', padding: '10px' }}
-								/>
-							}
-							actions={[
-								<Row justify={'space-between'}>
-									<Button
-										className={styles.rowBtn}
-										onClick={(e) => handleMyCourse(item.lessonId, e)}
-									>
-										课程详情
-									</Button>
-									<Button
-										className={styles.rowBtn}
-										onClick={(e) => handleMyDetail(item.lessonId, e)}
-									>
-										现在这是我的已选课程, 点击前往作业列表
-									</Button>
-								</Row>,
-							]}
-						>
-							<Meta
-								title={item.lessonName}
-								description={item.info}
-								style={{ height: '80px' }}
-							/>
+			<Row gutter={24}>
+				{model.map((item, index) => (
+					<Col span={24}>
+						<Card key={index} className={styles.card}>
+							<Meta title={item.name} style={{ height: '80px' }} />
+							<Row gutter={24}>
+								{modelLessons[item.name] || modelLessons['default']}
+							</Row>
 						</Card>
 					</Col>
 				))}
 			</Row>
-			<h1>主题二</h1>
-			<Divider />
 		</div>
 	);
 }
+
 // }
 //@ts-nocheck
 // import React, { useEffect, useState } from 'react';
