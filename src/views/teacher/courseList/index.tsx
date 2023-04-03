2f06ea57370d32a30a5184df3e0087a8574a80d0
IngaFred <116004221+IngaFred@users.noreply.github.com>
2023-04-03 16:11:28 +0800

    feat: 课程空组件

--- src/views/teacher/courseList/index.tsx
+++ src/views/teacher/courseList/index.tsx
@@ -1,44 +1,71 @@
 // @ts-nocheck
-import styles from "./index.module.scss";
-import React, { useEffect, useState } from "react";
-import { Card, Row, message, Col, Button, Select } from "antd";
-import { getCourseList } from "../../../service/courseList";
-import { useNavigate } from "react-router-dom";
-import Meta from "antd/es/card/Meta";
+import styles from './index.module.scss';
+import React, { useEffect, useState } from 'react';
+import { Card, Row, message, Col, Button, Select } from 'antd';
+import { getCourseList } from '../../../service/courseList';
+import { useNavigate } from 'react-router-dom';
+import Meta from 'antd/es/card/Meta';
 
 // 我的课程列表
 // 洪浩然
+const date = new Date();
 
 export default function List() {
-  const [lesson, setLesson] = useState([]);
+	const [year, setFullYear] = useState(date.getFullYear());
 
-  useEffect(() => {
-    getCourseList().then((ret) => {
-      if (ret.data.success) {
-        message.success(ret.data.errorMsg);
-        setLesson(ret.data.data);
-        //显示回参
-        //console.log(Courses);
-      } else {
-        message.error("获取课程失败");
-      }
-    });
-  }, []);
+	const [lesson, setLesson] = useState([]);
 
-  const navigate = useNavigate();
-  const handleMyCourse = (e: React.MouseEvent<HTMLButtonElement>) => {
-    navigate("/courseTeacher", { state: { lessonId: { e } } });
-  };
-  const handleMyDetail = (e: React.MouseEvent<HTMLButtonElement>) => {
-    navigate("/detailListTeacher", { state: { lessonId: { e } } });
-  };
+	useEffect(() => {
+		getCourseList().then((ret) => {
+			if (ret.data.success) {
+				message.success(ret.data.errorMsg);
+				setLesson(ret.data.data);
+				//显示回参
+				//console.log(Courses);
+			} else {
+				message.error('获取课程失败');
+			}
+		});
+	}, []);
 
-  return (
-    <div className={styles["big"]}>
-      <div className={styles["header-xxx"]}>
-        <h1>我的课程</h1>
+	const navigate = useNavigate();
+	const handleMyCourse = (e: React.MouseEvent<HTMLButtonElement>) => {
+		navigate('/courseTeacher', { state: { lessonId: { e } } });
+	};
+	const handleMyDetail = (e: React.MouseEvent<HTMLButtonElement>) => {
+		navigate('/detailListTeacher', { state: { lessonId: { e } } });
+	};
 
-        <Select
+	const yearOptions = [];
+	const putYear = () => {
+		for (let i = 2013; i < 11; i++) {
+			console.log(i);
+			yearOptions.push(
+				<Select.Option key={i} value={i}>
+					{i + 1}年
+				</Select.Option>
+			);
+		}
+	};
+
+	return (
+		<div className={styles['big']}>
+			<div className={styles['header-xxx']}>
+				<h1>我的课程</h1>
+				<Select
+					// 实时更改
+					value={year}
+					onChange={(newYear) => {
+						const now = value.clone().getFullYear(year);
+						setFullYear(newYear);
+						onChange(now);
+					}}
+				>
+					{putYear()}
+					{yearOptions}
+				</Select>
+
+				{/* <Select
           className={styles["st"]}
           showSearch
           mode="multiple"
@@ -78,47 +105,47 @@ export default function List() {
               label: "2023",
             },
           ]}
-        />
-      </div>
-      <Row gutter={(24, 8)} className={styles["row-big"]}>
-        {lesson.map((item, index) => (
-          <Col span={8} key={index}>
-            <Card
-              size="small"
-              className={styles.card}
-              cover={
-                <img
-                  src={item?.picUrl}
-                  alt=""
-                  style={{ width: "300px", height: "180px", padding: "10px" }}
-                />
-              }
-              actions={[
-                <Row justify={"space-between"}>
-                  <Button
-                    className={styles.rowBtn}
-                    onClick={(e) => handleMyCourse(item.lessonId, e)}
-                  >
-                    课程详情
-                  </Button>
-                  <Button
-                    className={styles.rowBtn}
-                    onClick={(e) => handleMyDetail(item.lessonId, e)}
-                  >
-                    作业列表
-                  </Button>
-                </Row>,
-              ]}
-            >
-              <Meta
-                title={item?.lessonName}
-                description={item?.info}
-                style={{ height: "80px" }}
-              />
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </div>
-  );
+        /> */}
+			</div>
+			<Row gutter={(24, 8)} className={styles['row-big']}>
+				{lesson.map((item, index) => (
+					<Col span={8} key={index}>
+						<Card
+							size="small"
+							className={styles.card}
+							cover={
+								<img
+									src={item?.picUrl}
+									alt=""
+									style={{ width: '300px', height: '180px', padding: '10px' }}
+								/>
+							}
+							actions={[
+								<Row justify={'space-between'}>
+									<Button
+										className={styles.rowBtn}
+										onClick={(e) => handleMyCourse(item.lessonId, e)}
+									>
+										课程详情
+									</Button>
+									<Button
+										className={styles.rowBtn}
+										onClick={(e) => handleMyDetail(item.lessonId, e)}
+									>
+										作业列表
+									</Button>
+								</Row>,
+							]}
+						>
+							<Meta
+								title={item?.lessonName}
+								description={item?.info}
+								style={{ height: '80px' }}
+							/>
+						</Card>
+					</Col>
+				))}
+			</Row>
+		</div>
+	);
 }
