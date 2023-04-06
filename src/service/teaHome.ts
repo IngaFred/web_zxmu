5f1460e2f2fc4b87cc5a25694b437790b871ea2f
IngaFred <116004221+IngaFred@users.noreply.github.com>
2023-04-06 18:04:42 +0800

    fix: 页面整体逻辑整理

---
+++ src/service/teaHome.ts
@@ -0,0 +1,18 @@
+import http from "../utils/http";
+
+// 获取主题模块
+export const getModel = async () => {
+  const ret = await http.get("/lesson/model");
+  return ret;
+};
+
+// 获取对应主题模块下的所有课程
+export const getModelLessons = async (modelId: string) => {
+  const ret = await http.get("/lesson/model/lesson?modelId=" + modelId);
+  return ret;
+};
+// 获取学生已选课程
+export const getMyLessons = async () => {
+  const ret = await http.get("/lesson/simple");
+  return ret;
+}
