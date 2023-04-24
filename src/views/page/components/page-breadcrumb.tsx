211ef80fe25b6ad447b2cbb456693043fdc7370d
626723063 <626723063@qq.com>
2023-04-24 22:33:27 +0800

    fix: 样式修复

--- src/views/page/components/page-breadcrumb.tsx
+++ src/views/page/components/page-breadcrumb.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { Breadcrumb } from "antd";
-import styles from "../index.module.scss";
+import React from 'react';
+import { Breadcrumb } from 'antd';
+import styles from '../index.module.scss';
 // 获取matches
-import { matchRoutes, useLocation } from "react-router-dom";
-import { routes } from "../../../router";
+import { matchRoutes, useLocation } from 'react-router-dom';
+import { routes } from '../../../router';
 
 export default function HomeBreadcrumb() {
   const location = useLocation();
@@ -11,10 +11,11 @@ export default function HomeBreadcrumb() {
   // items内部值是key+title
   const items = (matches ?? []).map((v) => ({
     key: v.pathnameBase,
-    title: v.route.meta?.title
+    title: v.route.meta?.title,
+    href: '#' + v.pathnameBase,
   }));
   return (
     /* Breadcrumb面包屑 */
-    <Breadcrumb className={styles["home-breadcrumb"]} items={items} />
+    <Breadcrumb className={styles['home-breadcrumb']} items={items} />
   );
 }
